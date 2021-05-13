d786671177d33c96bdb819d293d5aafea47ff05b
douglas86 <douglasmaxton@gmail.com>
2021-05-13 12:27:17 +0200

    Iterating over quotes with a map

--- src/components/HomePage/style.jsx
+++ src/components/HomePage/style.jsx
@@ -10,10 +10,19 @@ const useStyles = makeStyles({
     width: "20rem",
   },
   left: {
-    marginTop: "1rem",
+    marginTop: "8rem",
+    marginLeft: "10%",
   },
   leftHeading: {
     textAlign: "left",
+    fontFamily: "'Anton', sans-serif",
+    color: "#4e9151",
+  },
+  leftPTags: {
+    textAlign: "center",
+    fontFamily: "'Dancing Script', cursive;",
+    fontSize: "1.8rem",
+    color: "#415f66",
   },
 });
 
